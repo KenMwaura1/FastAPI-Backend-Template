7ce086a4d679460d3e5e5996dffe1816356b17d3
KenMwaura1 <kemwaura@gmail.com>
2025-12-15 14:29:50 +0300

    Fix HTTP 401 exception function: correct function name and update status code to 401 Unauthorized.

--- backend/src/utilities/exceptions/http/exc_401.py
+++ backend/src/utilities/exceptions/http/exc_401.py
@@ -8,8 +8,8 @@ import fastapi
 from src.utilities.messages.exceptions.http.exc_details import http_401_unauthorized_details
 
 
-async def http_exc_401_cunauthorized_request() -> Exception:
+async def http_exc_401_unauthorized_request() -> Exception:
     return fastapi.HTTPException(
-        status_code=fastapi.status.HTTP_400_BAD_REQUEST,
+        status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
         detail=http_401_unauthorized_details(),
     )
