c7d63d3be29b248dae9a872a137244683e6f6d6f
KenMwaura1 <kemwaura@gmail.com>
2025-12-15 14:29:45 +0300

    Rename function for HTTP 403 exception: update to consistent naming convention for clarity.

--- backend/src/utilities/exceptions/http/exc_403.py
+++ backend/src/utilities/exceptions/http/exc_403.py
@@ -7,7 +7,7 @@ import fastapi
 from src.utilities.messages.exceptions.http.exc_details import http_403_forbidden_details
 
 
-async def http_403_exc_forbidden_request() -> Exception:
+async def http_exc_403_forbidden_request() -> Exception:
     return fastapi.HTTPException(
         status_code=fastapi.status.HTTP_403_FORBIDDEN,
         detail=http_403_forbidden_details(),
